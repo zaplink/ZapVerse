c10def8127e85702b84f0eb3aa04b90ac3d18cb5
UdasriHasindu <usrimart@gmail.com>
2025-06-26 16:27:23 +0530

    add html templates

---
+++ src/main/java/com/zaplink/ZapVerse/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.zaplink.ZapVerse.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@AllArgsConstructor
+public class UserViewController {
+
+    
+
+}
