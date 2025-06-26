5f27a6f4a0c311c9512bfc0bef348a7314283d28
UdasriHasindu <usrimart@gmail.com>
2025-06-26 16:30:31 +0530

    check and authenticate user profiles

--- src/main/java/com/zaplink/ZapVerse/repository/ProfileRepository.java
+++ src/main/java/com/zaplink/ZapVerse/repository/ProfileRepository.java
@@ -4,6 +4,9 @@ import com.zaplink.ZapVerse.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+    Optional<Profile> findByEmail(String email);
 }
