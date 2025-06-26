cce8762ee9f6a5537f1b58e1b321d0b08889e204
UdasriHasindu <usrimart@gmail.com>
2025-06-26 08:56:17 +0530

    inital commit

---
+++ src/main/java/com/zaplink/ZapVerse/ZapVerseApplication.java
@@ -0,0 +1,13 @@
+package com.zaplink.ZapVerse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapVerseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapVerseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaplink/ZapVerse/ZapVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaplink.ZapVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
