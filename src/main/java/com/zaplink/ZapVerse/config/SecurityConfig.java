2ee7fb4ec6a8e76f9f22cad33816880153021b66
UdasriHasindu <usrimart@gmail.com>
2025-07-06 13:47:08 +0530

    setup security configuration : SecurityFilterChain

--- src/main/java/com/zaplink/ZapVerse/config/SecurityConfig.java
+++ src/main/java/com/zaplink/ZapVerse/config/SecurityConfig.java
@@ -1,4 +1,64 @@
 package com.zaplink.ZapVerse.config;
 
+import com.zaplink.ZapVerse.model.Profile;
+import com.zaplink.ZapVerse.repository.ProfileRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(ProfileRepository profileRepository){
+
+        return username -> {
+            Profile profile = profileRepository.findByEmail(username)
+                    .orElseThrow( () -> new UsernameNotFoundException("Email not found: " + username));
+
+            return org.springframework.security.core.userdetails.User
+                    .withUsername(profile.getEmail())
+                    .password(profile.getPassword())
+                    .roles("USER")
+                    .build();
+        };
+    }
+
+    // password hashing
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity https) throws Exception{
+
+        https
+                .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .formLogin( form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/feed", true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .permitAll()
+                );
+
+        return https.build();
+    }
 }
