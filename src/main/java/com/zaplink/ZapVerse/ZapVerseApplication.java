883f2c44c6128fb7f75cb30f88fe2f2b5abfdb0e
Pathum Madhusanka <pathum23200@gmail.com>
2025-07-07 12:13:30 +0530

    feat: connecet remote postgresql db

--- src/main/java/com/zaplink/ZapVerse/ZapVerseApplication.java
+++ src/main/java/com/zaplink/ZapVerse/ZapVerseApplication.java
@@ -2,11 +2,19 @@ package com.zaplink.ZapVerse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ZapVerseApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Set system properties so Spring Boot can resolve placeholders
+		System.setProperty("SUPABASE_DB_URL", dotenv.get("SUPABASE_DB_URL"));
+		System.setProperty("SUPABASE_DB_USERNAME", dotenv.get("SUPABASE_DB_USERNAME"));
+		System.setProperty("SUPABASE_DB_PASSWORD", dotenv.get("SUPABASE_DB_PASSWORD"));
+
 		SpringApplication.run(ZapVerseApplication.class, args);
 	}
 
