8b1d8860909d7fb2b3869d86379c26ad49563223
UdasriHasindu <usrimart@gmail.com>
2025-07-07 14:24:16 +0530

    set redirection between login and register pages

--- src/main/java/com/zaplink/ZapVerse/config/SecurityConfig.java
+++ src/main/java/com/zaplink/ZapVerse/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 
         https
                 .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/login", "/css/**").permitAll()
+                        .requestMatchers("/login", "/css/**", "/register", "/avatars/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
