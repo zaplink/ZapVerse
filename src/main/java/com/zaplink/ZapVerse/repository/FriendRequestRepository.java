f9d16edb04e1e97fea192851c72161c250d5e3f2
Sitharahansamali <hsithara69@gmail.com>
2025-07-09 16:54:15 +0530

    making friend suggesions, Friend-requests and All friends

--- src/main/java/com/zaplink/ZapVerse/repository/FriendRequestRepository.java
+++ src/main/java/com/zaplink/ZapVerse/repository/FriendRequestRepository.java
@@ -1,9 +1,18 @@
 package com.zaplink.ZapVerse.repository;
 
 import com.zaplink.ZapVerse.model.FriendRequest;
+import com.zaplink.ZapVerse.model.Profile;
+import com.zaplink.ZapVerse.model.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, Integer> {
+    List<FriendRequest> findByProfileAndReceiverAndStatus(Profile profile, Profile receiver, RequestStatus status);
+
+    List<FriendRequest> findByReceiverAndStatus(Profile receiver, RequestStatus status);
+
+    List<FriendRequest> findByProfileAndStatus(Profile profile, RequestStatus status);
 }
